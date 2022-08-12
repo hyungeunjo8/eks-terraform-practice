fbcb9a5646f3acdae33df000efed6e7acac223d4
hyungeunjo8 <hyungeunjo@weversecompany.com>
2022-08-12 14:17:57 +0900

    chore: secretsmanager key, value

--- _modules/secretsmanager/secretsmanager.tf
+++ _modules/secretsmanager/secretsmanager.tf
@@ -1,3 +1,18 @@
 resource "aws_secretsmanager_secret" "secretsmanager" {
   name = var.name
 }
+
+resource "aws_secretsmanager_secret_version" "example" {
+  secret_id     = aws_secretsmanager_secret.secretsmanager.id
+  secret_string = jsonencode(var.example)
+}
+
+variable "example" {
+  default = {
+    key1 = "value1"
+    key2 = "value2"
+  }
+
+  type = map(string)
+}
+
